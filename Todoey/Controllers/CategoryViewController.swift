7a63f6f5aae245c778c9d9ae354588b9d770b54d
Anand Agrawal <anandagrawal@anands-macbook-air.local>
2018-03-11 19:05:35 -0500

    persisting data using Core data completed

--- Todoey/Controllers/CategoryViewController.swift
+++ Todoey/Controllers/CategoryViewController.swift
@@ -11,7 +11,7 @@ import CoreData
 
 class CategoryViewController: UITableViewController {
     
-    var categoryArray = [Category]()
+    var categories = [Category]()
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
    
@@ -19,14 +19,9 @@ class CategoryViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//         let category1 = Category(context: context)
-//         category1.name = "Test123"
-//        categoryArray.append(category1)
-//        let category2 = Category(context: context)
-//        category2.name = "Test321"
-//        categoryArray.append(category2)
-      
-       loadCategoryData()
+
+        
+      loadCategories()
       
     }
 
@@ -36,18 +31,18 @@ class CategoryViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
-        cell.textLabel?.text = categoryArray[indexPath.row].name
+        cell.textLabel?.text = categories[indexPath.row].name
         
         return cell
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return categoryArray.count
+        return categories.count
     }
     
     //MARK: - Data Manipulation Methods
     
-    func saveCategoryData() {
+    func saveCategories() {
         
         do {
             try context.save()
@@ -59,17 +54,31 @@ class CategoryViewController: UITableViewController {
        
     }
     
-    func loadCategoryData (with request : NSFetchRequest<Category> = Category.fetchRequest()) {
+    func loadCategories (with request : NSFetchRequest<Category> = Category.fetchRequest()) {
         
         do {
-           categoryArray =  try context.fetch(request)
+           categories =  try context.fetch(request)
         } catch {
             print("Error fetching data \(error)")
         }
         tableView.reloadData()
     }
     
-    //MARK:- Add new Categorries
+     //MARK: - TableView Deligate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "showItems", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destinationVC = segue.destination as! TodoListViewController
+        if let indexPath = tableView.indexPathForSelectedRow {
+            destinationVC.selectedCategory = categories[indexPath.row]
+        }
+        
+    }
+    
+    //MARK:- Add new Category
 
     @IBAction func AddButtonPressed(_ sender: UIBarButtonItem) {
         
@@ -83,8 +92,8 @@ class CategoryViewController: UITableViewController {
         let action = UIAlertAction.init(title: "Add", style: .default) { (action) in
            let newCategory = Category(context: self.context)
             newCategory.name = textField.text!
-            self.categoryArray.append(newCategory)
-           self.saveCategoryData()
+            self.categories.append(newCategory)
+           self.saveCategories()
         }
    
         alert.addAction(action)
@@ -94,5 +103,5 @@ class CategoryViewController: UITableViewController {
     }
     
     
-    //MARK: - TableView Deligate Methods
+   
 }
